ad4b198c35f60ea81a23888a1a90f3cff4b5c675
Acer <logiv09@gmail.com>
2025-12-02 20:00:28 +0300

    6. Правила Checkstyle [#504981]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -1,24 +1,23 @@
 package ru.job4j.checkstyle;
 
 public class Broken {
-    String name;
-
-    Broken() {
-    }
-
-    void echo() {
-    }
-
     private int sizeOfEmpty = 10;
-    public String surname;
-    public static String newValue = "   ";
+    private String surname;
+    private String name;
+    private static String newValue = "   ";
 
-    void media(Object obj) {
+    public Broken() {
+    }
+
+    public void echo() {
+    }
+
+    public void method(int a, int b, int c, int d, int e, int f, int g) {
+    }
+
+    public void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
-    }
-
-    void method(int a, int b, int c, int d, int e, int f, int g) {
     }
 }
